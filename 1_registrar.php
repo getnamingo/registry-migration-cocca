d83b5db3db2a6ba854ae552096c27f866b827584
Pinga <121483313+getpinga@users.noreply.github.com>
2023-12-27 18:34:55 +0200

    Updated registrar import script

--- 1_registrar.php
+++ 1_registrar.php
@@ -17,23 +17,31 @@ try {
     die("Connection failed: " . $e->getMessage());
 }
 
-$inputFile = fopen('clients.csv', 'r');
+$inputFile = fopen('Clients.csv', 'r');
 $headers = fgetcsv($inputFile);
 
 while (($row = fgetcsv($inputFile)) !== false) {
     $data = array_combine($headers, $row);
-    
+
     // Inserting into the 'registrar' table
-    $sql = "INSERT INTO registrar (name, iana_id, clid, pw, prefix, email, whois_server, rdap_server, url, abuse_email, abuse_phone, accountBalance, creditLimit, creditThreshold, thresholdType, currency, crdate, update) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW(), NOW())";
+    $sql = "INSERT INTO registrar (name, clid, pw, prefix, email, whois_server, rdap_server, url, abuse_email, abuse_phone, accountBalance, creditLimit, creditThreshold, thresholdType, currency, crdate, lastupdate) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW(), NOW())";
     $stmt = $pdo->prepare($sql);
-    $stmt->execute([$data['Name'], $data['ROID'], 'example_clid', 'example_pw', 'EX', $data['E-mail'], 'whois.example.com', 'rdap.example.com', 'http://example.com', 'abuse@example.com', '0', 1000.00, 0.00, 0.00, 'fixed', 'USD']);
+    $stmt->execute([$data['Name'], $data['Login'], 'example_pw', 'EX', $data['E-mail'], 'whois.example.com', 'rdap.example.com', 'http://example.com', 'abuse@example.com', '0', 1000.00, 0.00, 0.00, 'fixed', 'USD']);
 
     $registrarId = $pdo->lastInsertId();
 
     // Inserting into the 'registrar_contact' table
-    $contactSql = "INSERT INTO registrar_contact (registrar_id, type, title, first_name, middle_name, last_name, org, street1, street2, street3, city, sp, pc, cc, voice, fax, email) VALUES (?, 'owner', ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    $contactSql = "INSERT INTO registrar_contact (registrar_id, type, first_name, last_name, city, cc, voice, email) VALUES (?, 'owner', ?, ?, ?, ?, ?, ?)";
     $contactStmt = $pdo->prepare($contactSql);
-    $contactStmt->execute([$registrarId, '', '', '', '', $data['Name'], $data['Address'], '', '', '', '', '', '', $data['Country'], $data['Phone'], $data['Fax'], $data['E-mail']]);
+    $contactStmt->execute([$registrarId, $data['Name'], $data['Name'], $data['Address'], $data['Country'], $data['Phone'], $data['E-mail']]);
+    
+    $contactSql = "INSERT INTO registrar_contact (registrar_id, type, first_name, last_name, city, cc, voice, email) VALUES (?, 'billing', ?, ?, ?, ?, ?, ?)";
+    $contactStmt = $pdo->prepare($contactSql);
+    $contactStmt->execute([$registrarId, $data['Billing Contact'], $data['Billing Contact'], $data['Address'], $data['Country'], $data['Phone'], $data['Billing E-mail']]);
+    
+    $contactSql = "INSERT INTO registrar_contact (registrar_id, type, first_name, last_name, city, cc, voice, email) VALUES (?, 'tech', ?, ?, ?, ?, ?, ?)";
+    $contactStmt = $pdo->prepare($contactSql);
+    $contactStmt->execute([$registrarId, $data['Technical Contact'], $data['Technical Contact'], $data['Address'], $data['Country'], $data['Phone'], $data['Technical E-mail']]);
 }
 
 fclose($inputFile);
